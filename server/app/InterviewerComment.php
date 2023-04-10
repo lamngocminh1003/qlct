165ee090ccb5539b0dfbf3638fd815a8db7bed8c
Ngọc Minh <lamngocminh1003@gmail.com>
2023-04-10 14:47:41 +0700

    create migration lavarel

---
+++ server/app/InterviewerComment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InterviewerComment extends Model
+{
+    protected $table = 'interviewer_comment';
+    protected $fillable = ['id','del_flag', 'interviewer_id', 'comment','rating',
+    'created_at','updated_at'];
+}
