acf0c16fedb2a1bf86fb7233f76851f6bcfd4221
Ngọc Minh <lamngocminh1003@gmail.com>
2023-04-12 11:03:09 +0700

    create seeder lavarel

--- server/database/seeds/DatabaseSeeder.php
+++ server/database/seeds/DatabaseSeeder.php
@@ -5,12 +5,21 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(ConfigurationSeeder::class);
+        $this->call(MemberTableSeeder::class);
+        $this->call(TeamTableSeeder::class);
+        $this->call(TeamMemberTableSeeder::class);
+        $this->call(CandidateTableSeeder::class);
+        $this->call(CandidateSkillTableSeeder::class);
+        $this->call(CandidateContactTableSeeder::class);
+        $this->call(InterviewTableSeeder::class);
+        $this->call(InterviewerTableSeeder::class);
+        $this->call(InterviewerCommentTableSeeder::class);
     }
 }
